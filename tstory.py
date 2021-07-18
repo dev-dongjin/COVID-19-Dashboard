f17f6ed31ccdcffbc8b32f22215cdc123106e724
Your Name <andyd19@naver.com>
2021-07-18 16:48:56 +0900

    [1.1.0] updated

--- tstory.py
+++ tstory.py
@@ -5,23 +5,22 @@ import webbrowser
 # import chroller as chrolling
 
 
-#client_id  == app_id
-client_id = "3a11d2b35805b3423cc23f3bdf53f7b8"
+# client_id  == app_id
+client_id = ""
 #client_secret = Secret_Key
-Secret_Key = "3a11d2b35805b3423cc23f3bdf53f7b8a4008091eeae735222ddd056c8740101d2229515"
-#code는 access_token을 받기 위한 중간 인증코드, 나중에 사용하지 않음.
-code = "7a6d5ebe97ff0b13616dee326c00976e1f3f83ba7eb2ec6660fa5bb12e000c6d80e16384"
-#access_token는 이후에 모든 인증에서 사용하는 최종 키임.
-access_token = "33a68a64f9a30ad458f9a21ef6007eb6_fa4950a92d3ad1a426cd0c8621a63b06"
+Secret_Key = ""
+# code는 access_token을 받기 위한 중간 인증코드, 나중에 사용하지 않음.
+code = ""
+# access_token는 이후에 모든 인증에서 사용하는 최종 키임.
+access_token = ""
 
-#티스토리 주소의 아이디나 전체주소
+# 티스토리 주소의 아이디나 전체주소
 blog_name = "https://digitalnomad-lee.tistory.com/"
 
-#콜백주소는 자신의 티스토리 주소를 사용해도 되고, 끝에 /는 넣지 않는다.
+# 콜백주소는 자신의 티스토리 주소를 사용해도 되고, 끝에 /는 넣지 않는다.
 redirect_uri = "https://digitalnomad-lee.tistory.com"
 state_param = ""
 output_type = "json"
-
 
 
 def init():
@@ -39,19 +38,21 @@ def init():
         print('r의 값은 : '+r.text)
 
     # 이제 실제 사용할 액세스 코드를 가지고 호출을 할 수 있다.
- 
+
+
 def get_info():
     # info_url = f'https://www.tistory.com/apis/blog/info?access_token={access_token}&output={output_type}'
     info_url = "https://www.tistory.com/apis/blog/info?access_token=33a68a64f9a30ad458f9a21ef6007eb6_fa4950a92d3ad1a426cd0c8621a63b06&output=json"
-    r =requests.get(info_url)
+    r = requests.get(info_url)
     print(r.text)
     return r.text
+
 
 def get_list(page_number):
     url = f'https://www.tistory.com/apis/post/list?access_token={access_token}&output={output_type}&blogName={blog_name}&page={page_number}'
     print(url)
     r = requests.get(url)
-    print(r.text)    
+    print(r.text)
     return r
 
 
@@ -64,7 +65,7 @@ def get_category():
         print('category의 값 가져오기')
         print(i)
         print(f'{i["name"]} ({i["id"]})')
-    
+
     return r
 
 
@@ -75,7 +76,6 @@ def get_read(new_post_id):
     r = requests.get(url)
     print(r.text)
     return(r)
-
 
 
 # def get_write(new_content):
@@ -94,14 +94,14 @@ def get_read(new_post_id):
 #     print(r, r.text)
 #     file_handle.saveURL(r.json()['tistory']['url'])
 #     return r.json()['tistory']['url']
-    
+
 def file_write(capture):
-    title = "테스트4" #제목 (필수)
+    title = "테스트4"  # 제목 (필수)
     content = ''
-    visibility = "0" #발행상태 0비공개-기본, 1보고,3발행
-    category_id = "0" #카테고리 아이디 기본값 0
-    slogan = "" #문자주소
-    tag = "" #태그 ,로 구분
+    visibility = "0"  # 발행상태 0비공개-기본, 1보고,3발행
+    category_id = "0"  # 카테고리 아이디 기본값 0
+    slogan = ""  # 문자주소
+    tag = ""  # 태그 ,로 구분
     files = {'uploadedfile': open(f'./data/{capture}.png', 'rb')}
     url = f'https://www.tistory.com/apis/post/attach?access_token={access_token}&blogName={blog_name}&title={title}&content={content}&visibility={visibility}&category={category_id}&slogan={slogan}&tag={tag}&output={output_type}'
     r = requests.post(url, files=files)
@@ -109,4 +109,3 @@ def file_write(capture):
     print(r.url)
     print(r.json()['tistory']['url'])
     return r.json()['tistory']['url']
-    
